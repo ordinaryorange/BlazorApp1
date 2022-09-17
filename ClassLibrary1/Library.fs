d9f74138da12399df142eb39c600ed4cbdfad615
OrdinaryOrange <ordinaryorange@users.noreply.github.com>
2022-09-17 10:22:49 +0930

    Add project files.

---
+++ ClassLibrary1/Library.fs
@@ -0,0 +1,13 @@
+﻿namespace ClassLibrary1
+
+open FSharp.Control
+
+
+
+type Tester() =
+    static member DoAsyncSeq () =
+        task {
+            let! x = AsyncSeq.init 10 id |> AsyncSeq.toAsyncEnum |> AsyncSeq.ofAsyncEnum |> AsyncSeq.length
+            return x
+        }
+        
